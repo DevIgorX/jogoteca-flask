ec0ce7a0836a7b23e137f537cf7ce73c8896ffc6
tauam <tauam.pinheiro@goias.gov.br>
2025-09-16 17:49:02 -0300

    corrigindo de arquivo de configuração

--- config.py
+++ config.py
@@ -1,2 +1,10 @@
+
+import os
+
+# Pega o caminho absoluto para o diretório onde este arquivo (config.py) está.
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 SECRET_KEY = 'alura'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///jogoteca.db' 
+
+# Cria o caminho absoluto para o arquivo do banco de dados dentro do diretório do projeto.
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'jogoteca.db')
