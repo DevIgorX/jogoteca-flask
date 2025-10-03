e0dfd09dcf6916d95c1a668454f360b44e55f0b5
tauam <tauam.pinheiro@goias.gov.br>
2025-10-03 17:58:59 -0300

    adicionando arquivo de imagens

--- routes.py
+++ routes.py
@@ -33,6 +33,9 @@ def criar():
     db.session.add(novo_jogo)
     db.session.commit()
 
+    arquivo = request.files['arquivo']
+    arquivo.save(f'uploads/{arquivo.filename}')
+
     return redirect(url_for('rotas.index'))
 
 @rotas.route('/login')
